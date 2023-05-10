ff3f0a7861196cdfa58996b5792b2ab4a15cccf7
Davi Soares <davi.soares@psatech.com.br>
2023-05-09 22:42:43 -0300

    button header mock made 🤭

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,40 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native"
 
-const Header = () => {
+type Props = {
+    isHomePage: boolean,
+    title: string,
+    subtitle: string
+}
+
+const Header = ({ isHomePage, title, subtitle }: Props) => {
+
+    isHomePage = true
+    if (isHomePage) {
+        title = "Beatriz Seco Corrêa - 4º ano"
+        subtitle = "Engenharia Civil - 5 anos - Verão - Diurno"
+    }
+    else {
+        title = "Teoria das estruturas"
+        subtitle = "ETC314"
+    }
+
     return <View style={styles.content}>
         <View style={styles.bluelayer}>
-            <Text style={styles.mainInfosStudent}>Beatriz Seco Corrêa - 4º ano</Text>
-            <Text style={styles.infosStudent}>Engenharia Civil - 5 anos - Verão - Diurno</Text>
+            <View style={styles.texts}>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.subtitle}>{subtitle}</Text>
+            </View>
+            {
+                isHomePage ?
+                    null :
+                    <TouchableHighlight onPress={() => alert('volta pagina')}>
+                        <View style={styles.buttonExitContainer}>
+                            <Text style={styles.buttonExit}>
+                                X
+                            </Text>
+                        </View>
+                    </TouchableHighlight>
+            }
         </View>
         <View style={styles.redlayer} />
     </View >
@@ -15,27 +45,41 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
         flexDirection: "column",
-        alignItems: "center"
+        alignItems: "center",
     },
     bluelayer: {
+        flexDirection: "row",
         backgroundColor: "#0F5F88",
         width: "90%",
         height: "12%",
         borderRadius: 20,
         zIndex: 1,
         paddingHorizontal: "3%",
-        paddingVertical: "6%",
         borderWidth: 1.2,
     },
-    mainInfosStudent: {
+    texts: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    title: {
         color: "#fff",
         fontSize: 24,
         fontWeight: "bold",
     },
-    infosStudent: {
+    subtitle: {
         color: "#fff",
         fontSize: 13,
         fontWeight: "bold"
+    },
+    buttonExitContainer: {
+        width: "100%",
+        marginRight: "2%",
+        height: "100%",
+        justifyContent: "center"
+    },
+    buttonExit: {
+        fontSize: 30,
+        color: "#fff",
     },
     redlayer: {
         backgroundColor: "#BA2512",
