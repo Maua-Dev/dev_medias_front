b73d349a7c2c9fa7600600d7e182481a60daf920
Davi Soares <davi.soares@psatech.com.br>
2023-05-11 21:40:41 -0300

    turning less monkish 🙈🦍🆗?

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -5,20 +5,62 @@ import MainBox from "../../components/MainBox/MainBox"
 import SubjectCard from "../../components/SubjectCard/SubjectCard"
 
 const MainPage = () => {
+
+    const response = [
+        {
+            title: "Empreendedorismo e Gestão",
+            subtitle: "EFH113 - Diurno - Anual",
+            grade: 2
+        },
+        {
+            title: "Urbanismo",
+            subtitle: "ETC102 - Diurno - Anual",
+            grade: 7
+        },
+        {
+            title: "Projeto de Rodovias e Vias Urbanas",
+            subtitle: "ETC215 - Noturno - Anual",
+            grade: 2
+        },
+        {
+            title: "Estruturas de Concreto",
+            subtitle: "ETC323 - Diurno - Anual",
+            grade: 2
+        },
+        {
+            title: "Estruturas Metálicas e de Madeiras",
+            subtitle: "ETC326 - Diurno - Anual",
+            grade: 2
+        },
+        {
+            title: "Fenômeno de Transporte",
+            subtitle: "ETC411 - Diurno - Anual - DP",
+            grade: 2
+        },
+        {
+            title: "Hidráulica",
+            subtitle: "ETC413 - Noturno - Anual",
+            grade: 2
+        },
+        {
+            title: "Instalações Prediais Hidráulicas",
+            subtitle: "ETC415 - Diurno - Anual",
+            grade: 2
+        },
+        {
+            title: "Fundações",
+            subtitle: "ETC510 - Noturno - Anual",
+            grade: 2
+        },
+    ]
+
     return <>
         <Header isHomePage={true} />
         <MainBox>
-            <ScrollView>
-                <SubjectCard title={"Empreendedorismo e Gestão"} subtitle={"EFH113 - Diurno - Anual"} grade={2} />
-                <SubjectCard title={"Urbanismo"} subtitle={"ETC102 - Diurno - Anual"} grade={7} />
-                <SubjectCard title={"Projeto de Rodovias e Vias Urbanas"} subtitle={"ETC215 - Noturno - Anual"} grade={2} />
-                <SubjectCard title={"Teoria das Estruturas"} subtitle={"ETC314 - Diruno - Anual - DP"} grade={2} />
-                <SubjectCard title={"Estruturas de Concreto"} subtitle={"ETC323 - Diurno - Anual"} grade={2} />
-                <SubjectCard title={"Estruturas Metálicas e de Madeiras"} subtitle={"ETC326 - Diurno - Anual"} grade={2} />
-                <SubjectCard title={"Fenômeno de Transporte"} subtitle={"ETC411 - Diurno - Anual - DP"} grade={2} />
-                <SubjectCard title={"Hidráulica"} subtitle={"ETC413 - Noturno - Anual"} grade={2} />
-                <SubjectCard title={"Instalações Prediais Hidráulicas"} subtitle={"ETC415 - Diurno - Anual"} grade={2} />
-                <SubjectCard title={"Fundações"} subtitle={"ETC510 - Noturno - Anual"} grade={2} />
+            <ScrollView style={{ overflow: "hidden" }}>
+                {response.map((value, index) => {
+                    return <SubjectCard key={index} title={value.title} subtitle={value.subtitle} grade={value.grade} />
+                })}
             </ScrollView>
         </MainBox>
         <DevLogo />
